52ae622df79cbfb84ca51fbc0020dd019bb900c3
Todor Balabanov <todor.balabanov@gmail.com>
2025-08-13 14:20:30 +0300

    Board filling has been done.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,10 @@ class Tile {
 }
 
 class Board {
+	private static readonly Random PRNG = new Random();
+
 	private static Tile[] colors = {
+		new Tile(' '), 
 		new Tile('@'), 
 		new Tile('#'), 
 		new Tile('$'), 
@@ -48,12 +51,65 @@ class Board {
 		Width = width;
 		Height = height;
 		tiles = new Tile[Width][];
+		for(int w=0; w<Width; w++) {
+			tiles[w] = new Tile[ Height ];
+		}
+		for(int w=0; w<Width; w++) {
+			for(int h=0; h<Height; h++) {
+				tiles[w][h] = colors[0];
+			}
+		}
+	}
+
+	public void reset() {
+		for(int h=0; h<Height; h++) {
+			for(int w=0; w<Width; w++) {
+				int index = PRNG.Next(1, colors.Length-1);
+				tiles[w][h] = colors[ index ];
+			}
+		}
+		
+		while(tiles[0][0] == tiles[Width-1][Height-1]) {
+			int index = PRNG.Next(1, colors.Length-1);
+			tiles[0][0] = colors[ index ];
+		}
+	}
+	
+	public override string ToString(){
+		string result = "";
+		
+		for(int h=0; h<Height; h++) {
+			for(int w=0; w<Width; w++) {
+				result += tiles[w][h];
+			}
+			result += "\n";
+		}
+		
+		for(int c=1; c<colors.Length; c++) {
+			bool used = false;
+
+			if(tiles[0][0] == colors[c]) {
+				used = true;
+			}
+			if(tiles[Width-1][Height-1] == colors[c]) {
+				used = true;
+			}
+
+			if(used == true) {
+				result += "[" + colors[c] + "]";
+			} else {		
+				result += " " + colors[c] + " ";
+			}
+		}
+		
+		return result.Trim();
 	}
 }
 
 public class Program {
 	public static void Main(string[] args) {
-		Tile t1 = new Tile('#');
-		Console.WriteLine(t1);
+		Board board = new Board(13,7);
+		board.reset();
+		Console.WriteLine(board);
 	}
 }
